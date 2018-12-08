c68f4116d5ebc689b6f7f99fd43de3ee11d96ae7
SimpleCodeCX <248200851@qq.com>
2018-12-08 23:52:58 +0800

    exercise11 test passed

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,77 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 // 这个在 npm run dev 和 npm run build 时候是不同的
 var TARGET = process.env.npm_lifecycle_event
 var APP_PATH = path.join(__dirname, '/src')
+
+// const devWebpack = {
+//   mode: 'development',
+//   entry: path.join(APP_PATH, 'index.js'),
+//   output: {
+//     path: path.join(__dirname, 'dist'),
+//     filename: 'main.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       }
+//     ]
+//   },
+//   plugins: []
+// };
+
+// const buildWebpack = {
+//   mode: 'production',
+//   entry: path.join(APP_PATH, 'index.js'),
+//   output: {
+//     path: path.join(__dirname, 'dist'),
+//     filename: 'main.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ExtractTextPlugin.extract({
+//           fallback: 'style-loader',
+//           use: ['css-loader']
+//         })
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new ExtractTextPlugin({
+//       filename: '[name].css'
+//     })
+//   ]
+// }
+
+// module.exports = buildWebpack;
+
+module.exports = {
+  mode: TARGET === 'build' ? 'production' : 'development',
+  entry: path.join(APP_PATH, 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: TARGET === 'build'
+          ? ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+          : ['style-loader', 'css-loader']
+      },
+    ]
+  },
+  plugins: TARGET === 'build'
+    ? [
+      new ExtractTextPlugin({
+        filename: '[name].css'
+      })
+    ]
+    : []
+}
